615a3991275779a99e2c1f21cab8f71c6858b90e
ashutosh-mishra-git <ashutoshm610@gmail.com>
2022-12-29 20:31:11 +0530

    final
    final commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import {useState,useEffect} from 'react';
+import './App.css';
+import Moviecard from './Moviecard';
+import search from './search.svg'
+
+
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
+
+
+function App() {
+const [searchterm,setsearchterm] = useState("");
+const [setMovie,setMovieset] = useState([]);
+
+
+
+const searchMovie = async(title) =>{
+  const res = await fetch(`${API_URL}&s=${title}`);
+  const data = await res.json();
+  console.log(data.Response);
+  data.Response!=="False"?setMovieset(data.Search):setMovieset([]);
+  console.log(setMovie);
+ 
+
+}
+ 
+useEffect(()=>{
+  searchMovie("Batman")
+},[])
+
+ console.log(searchterm);
+  return (
+    <div className="app">
+      <h1>FilmWallah</h1>
+
+<div className="search">
+           <input value={searchterm}
+           onChange={(e)=>setsearchterm(e.target.value)}
+           placeholder="Search Movie"
+           />
+        
+           <img
+           src= {search}
+           onClick={()=>searchMovie(searchterm)}
+           />
+           </div>
+        { setMovie.length>0?(      
+        <div className='display__card'>
+           {setMovie.map((movie)=>{
+           return <Moviecard movie={movie} />
+           })}
+        </div>
+  ):(
+     <div className='empty'>
+    <h2>  Movie Not found</h2>
+     </div>
+  )
+}
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Moviecard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Moviecard(props) {
+  
+   const {Title,Year,Type,Poster,imdbID} = props.movie;
+   console.log(Poster);
+  return (
+    <div className='movie' key={imdbID}>
+        <div>
+        <p>{Year}</p>
+      </div>
+        <div>
+        <img src={Poster} alt="" />
+        </div>
+        
+
+        <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Moviecard
